b9280e13d38410b0de72f191fab63cc678f93152
YLF <1463020554@qq.com>
2021-11-10 17:41:42 +0800

    add 支持路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom'
+import TimeTotal from "./views/TimeTotal";
 
 function App() {
-  return <>App</>
+  return <div className="app">
+    <Routes>
+      <Route path='/time-total' element={<TimeTotal/>}/>
+    </Routes>
+  </div>
 }
 
 export default App;
---
+++ src/views/TimeTotal/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TimeTotal():React.ReactElement{
+    return <div>
+        TimeTotal
+    </div>
+}
