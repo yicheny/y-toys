8f2af2ea0a06b0f4b954a5a895e2c0404ad61b55
YLF <1463020554@qq.com>
2021-11-14 21:22:28 +0800

    add Menu组件支持固定【含自动浮现】功能

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -4,12 +4,21 @@ import clsx from "clsx";
 
 interface IconProps{
     name:string,
+    size?:number,
+    color?:string,
+    className?:string,
+    style?:React.CSSProperties,
     onClick?:React.MouseEventHandler<HTMLElement>
 }
 
 const Icon:React.FC<IconProps> = function (props){
-    return <i className={clsx('iconfont',`icon-${props.name}`)}
+    return <i className={clsx('iconfont',`icon-${props.name}`,props.className)}
+              style={{fontSize:props.size,color:props.color,...props.style}}
               onClick={props.onClick}/>
+}
+Icon.defaultProps = {
+    size:16,
+    color:'rgba(0,0,0,0.72)'
 }
 
 export default Icon;
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import './index.scss'
 import clsx from "clsx";
+import {Icon} from "../index";
+import _ from 'lodash'
 
 type MenuOption = {
     text:string,
@@ -16,17 +18,53 @@ interface MenuProps{
 
 export default function Menu(props:MenuProps):JSX.Element{
     const {pathname} = useLocation();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [shrink,setShrink] = useState<boolean>(true);
+    const [autoShrink,setAutoShrink] = useState<boolean>(true);
 
-    return <div className={clsx('c-menu',props.className)} style={props.style}>
-        {
-            props.options.map((option,key) => {
-                return <div className={clsx('c-menu-item',{current:pathname===option.to})}
-                            onClick={()=>navigate(option.to)}
-                            key={key}>
-                    {option.text}
-                </div>
-            })
+    const autoShrinkHandle = useCallback((x)=>{
+        if(x<=248){
+            // console.log(x)
+            setAutoShrink(false)
+        }else{
+            setAutoShrink(true)
         }
+    },[])
+
+    useMonitor(autoShrinkHandle)
+
+    return <div className={clsx('c-menu',props.className,{shrink:(shrink && autoShrink)})} style={props.style}>
+        <div className="c-menu-content">
+            {
+                props.options.map((option,key) => {
+                    return <div className={clsx('c-menu-item',{current:pathname===option.to})}
+                                onClick={()=>navigate(option.to)}
+                                key={key}>
+                        {option.text}
+                    </div>
+                })
+            }
+        </div>
+        <div className="c-menu-footer">
+            <Icon name={'fixed'}
+                  size={24}
+                  className={clsx({shrink})}
+                  style={{cursor:"pointer"}}
+                  onClick={()=>setShrink(!shrink)}/>
+        </div>
     </div>
+}
+
+function useMonitor(handle:(x:number,y:number)=>void){
+    useEffect(()=>{
+        const listener = function (event:MouseEvent){
+            handle(event.clientX,event.clientX)
+        }
+        const throttleListener = _.throttle(listener,60)
+        document.addEventListener('mousemove',throttleListener);
+
+        return ()=>{
+            document.removeEventListener('mousemove',throttleListener)
+        }
+    },[handle])
 }
