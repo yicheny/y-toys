b9e53245c5c7bd439875e8a51cc0ceb286875b21
yanglvfeng <ylvfeng@gmail.com>
2022-03-01 14:54:59 +0800

    update Menu收缩模式UI修改

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import './index.scss'
 import clsx from "clsx";
+import {useToggle} from "../../hooks";
 import {Button} from "../index";
 
 type MenuOption = {
@@ -25,27 +26,36 @@ Menu.defaultProps = {
 export default function Menu(props: MenuProps): JSX.Element {
     const {pathname} = useLocation();
     const navigate = useNavigate();
-    const [shrink, setShrink] = useState<boolean>(props.defaultShrink);
+    const { show:shrink, open:toShrink, close:toExpand } = useToggle(props.defaultShrink);
 
-    return <div className={clsx('c-menu', props.className, {shrink})} style={props.style}>
-        <Button onClick={() => setShrink(!shrink)}>{shrink ? props.expandText : props.shrinkText}</Button>
-        <div className="c-menu-content">
-            {
-                props.options.map((option, key) => {
-                    return <div className={clsx('c-menu-item', {current: pathname === option.to})}
-                                onClick={() => navigate(option.to)}
-                                key={key}>
-                        {shrink ? option.text.slice(0, 3).toLocaleUpperCase() : option.text}
-                    </div>
-                })
-            }
+    return <>
+        <div className={clsx('c-menu', props.className, {shrink})} style={props.style}>
+            <div className="c-menu-header">
+                <Button onClick={toShrink}>{props.shrinkText}</Button>
+                {/*<Icon name={'fixed'}
+                      size={24}
+                      className={clsx({shrink})}
+                      style={{cursor: "pointer"}}
+                      onClick={toggleShrink}/>*/}
+            </div>
+            <div className="c-menu-content">
+                {
+                    props.options.map((option, key) => {
+                        return <div className={clsx('c-menu-item', {current: pathname === option.to})}
+                                    onClick={() => navigate(option.to)}
+                                    key={key}>
+                            {option.text}
+                        </div>
+                    })
+                }
+            </div>
         </div>
-        {/*<div className="c-menu-footer">
-            <Icon name={'fixed'}
-                  size={24}
-                  className={clsx({shrink})}
-                  style={{cursor: "pointer"}}
-                  onClick={() => setShrink(!shrink)}/>
-        </div>*/}
-    </div>
+        <div className={clsx('c-menu-shrink',{expand:!shrink})}>
+            <div className="c-menu-shrink-btn"
+                 onClick={toExpand}
+            >
+                {props.expandText}
+            </div>
+        </div>
+    </>
 }
